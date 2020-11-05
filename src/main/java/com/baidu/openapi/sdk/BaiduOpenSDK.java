a82154ab50195480ceeb7e49690146ffdbd75ec3
your name <your email>
2020-11-05 23:29:04 +0800

    .

---
+++ src/main/java/com/baidu/openapi/sdk/BaiduOpenSDK.java
@@ -0,0 +1,10 @@
+package com.baidu.openapi.sdk;
+
+import com.baidu.openapi.BaiduOpenAPI;
+
+public class BaiduOpenSDK extends BaiduOpenAPI {
+    @Override
+    public smartapp$getunionid_response smartapp$getunionid(String access_token, smartapp$getunionid_body body) {
+        return null;
+    }
+}
---
+++ src/main/java/com/baidu/spapi/sdk/BaiduSpSDK.java
@@ -0,0 +1,11 @@
+package com.baidu.spapi.sdk;
+
+import com.baidu.spapi.BaiduSpAPI;
+
+public class BaiduSpSDK extends BaiduSpAPI {
+
+    @Override
+    public oauth$jscode2sessionkey_response oauth$jscode2sessionkey(String Content_Type, oauth$jscode2sessionkey_body body) throws Error {
+        return null;
+    }
+}
